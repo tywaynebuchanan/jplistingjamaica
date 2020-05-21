7c314b925bb37f04838bb7c6a809dd6cada6efca
Tywayne <tywaynebuchanan@gmail.com>
2020-05-21 16:01:29 -0500

    changes made

--- loginphp/searchresults.php
+++ loginphp/searchresults.php
@@ -60,10 +60,4 @@ if(!isset($_SESSION['login_user'])){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
-
-
-
-<div class="footer-tm">
-  <p>Footer</p>
-</div>
 </html>
